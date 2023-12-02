11ffabcd9bfd83f8acbcee09a8e8284b3c34ab21
Adrián Fonseca Vega <afonsecav88@gmail.com>
2023-12-01 21:32:42 -0300

    Create AddTask component

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { TodoApp } from './App.tsx';
 import './styles.css';
+import { AddTask } from './components/AddTask.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <TodoApp />
+    <AddTask />
   </React.StrictMode>
 );
