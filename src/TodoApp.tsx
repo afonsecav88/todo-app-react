05199c36f880b32b0ec64aea035e6e6aa25d1e01
Adrián Fonseca Vega <afonsecav88@gmail.com>
2023-12-03 17:10:43 -0300

    home page background-color changed

--- src/TodoApp.tsx
+++ src/TodoApp.tsx
@@ -10,7 +10,12 @@ export const TodoApp = () => {
   return (
     <TodoContext.Provider value={state}>
       <TodoDispatchContext.Provider value={dispatch}>
-        <h1 className="m-2">Lista de tareas:</h1>
+        <h1 className="m-4">Lista de tareas
+          <span className="top-0 start-100 translate-middle badge rounded-pill bg-success m-4 ">
+            {state.length}
+            <span className="visually-hidden">unread messages</span>
+          </span>
+        </h1>
         <hr />
         <div className="col d-flex gap-2 justify-content-around flex-wrap">
           <div className="row-md-8">
@@ -21,6 +26,6 @@ export const TodoApp = () => {
           </div>
         </div>
       </TodoDispatchContext.Provider>
-    </TodoContext.Provider>
+    </TodoContext.Provider >
   );
 };
